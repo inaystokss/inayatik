523c1f9ced205b84af7596740acc62fe31882731
Kabylashimova Inayat <champ@MacBook-Air-Kabylashimova-inayat.local>
2025-06-10 16:11:51 +0500

    Fix config: add missing comma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  target: 'static'
+  target: 'static',
 compatibilityDate: '2024-11-01',
 devtools: { enabled:true},
 modules: ['@nuxtjs/tailwindcss']
