9ace30f5d6e3028a43062689179c67b06f260f9c
Kabylashimova Inayat <champ@MacBook-Air-Kabylashimova-inayat.local>
2025-06-10 16:08:17 +0500

    Fix Nuxt config for static build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  target: 'static'
 compatibilityDate: '2024-11-01',
 devtools: { enabled:true},
 modules: ['@nuxtjs/tailwindcss']
